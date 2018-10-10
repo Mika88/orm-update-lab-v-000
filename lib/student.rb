e5c7bfdf8cc1566cd29a517716bfbec471da13aa
Mika88 <mikaspivak@gmail.com>
2018-10-10 20:42:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,8 @@ class Student
       LIMIT 1;
     SQL
 
-    DB[conn:].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
   end
-end
