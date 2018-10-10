f375fe80e9fb6fa1b1263ac1dbfc05b1638f3b65
Mika88 <mikaspivak@gmail.com>
2018-10-10 21:02:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,4 +52,9 @@ class Student
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
+
+  def update
+    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
 end
